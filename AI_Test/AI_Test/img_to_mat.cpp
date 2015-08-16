5860d7b1551ade027ca8824c8fd753a7632429f9
MattDN93 <matthew.deneef@gmail.com>
2015-08-17 00:40:23 +0200

    used example at: http://answers.opencv.org/question/4423/how-to-create-a-binary-image-mat/
    *Code opens image and creates binary version of it
    *Quits auto in 15sec
    *Run .exe from command line with filename as flag

--- AI_Test/AI_Test/img_to_mat.cpp
+++ AI_Test/AI_Test/img_to_mat.cpp
@@ -5,3 +5,54 @@
 */
 
 #include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;		//for ease using openCV namespace
+						//stdlib fns will be ref'd explicitly
+
+/* The user calls this exe with arguments for the filename*/
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) //if the user spec'd argument is wrong
+	{
+		std::cout << "Syntax error. Use '" << argv[0] << " <path_to_file>. Try again." << std::endl;
+		return 1;	//throw a syntax error and quit
+	}
+
+
+	/* Expect argument as "img_to_mat.exe C:\my\directory\filename.jpg" */
+
+	/*use CV libraries to deal with image
+		imread function takes any colour depth image with filename in argv
+	*/
+
+	Mat inputImgMat = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	if (inputImgMat.empty())
+	{
+		std::cerr << "Error. Image filename '" << argv[1] << "' is invalid or could not be opened." << std::endl;
+		return 1;
+	}
+
+	//convert the image matrix to greyscale (basically resize it)
+	Mat inputImgGreyScale(inputImgMat.size(), CV_8U);
+
+	//convert the image matrix to a type of matrix defined by the greyscale matrix
+	cvtColor(inputImgMat, inputImgGreyScale, CV_BGR2GRAY);
+
+	//binary matrix of values from image
+	Mat binaryMat(inputImgGreyScale.size(), inputImgGreyScale.type());
+
+	//apply threshold values (everything either goes to 0 or 255)
+	threshold(inputImgGreyScale, binaryMat, 100, 255, cv::THRESH_BINARY);
+
+	//display the results using OCV's window render
+	namedWindow("Binary matrix", WINDOW_AUTOSIZE);
+	imshow("Binary Matrix", binaryMat);
+
+	std::cout << "Done. Press any key to quit, else quitting in 15 seconds." << std::endl;
+
+	waitKey(15000);
+
+	return 0;
+}
